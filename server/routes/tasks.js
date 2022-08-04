f8422c011fd83ee9e2a88bdf3aa118f1e177f2cb
avishkakavindu <avishkakavindud@gmail.com>
2022-08-04 16:22:11 +0530

    Update task function

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -28,5 +28,18 @@ router.get("/", async(req, res) =>{
     } 
  });
  
+ // edit task
+ router.put("/:id", async(req, res) => {
+    try {
+        const task = await Task.findOneAndUpdate(
+            {_id: req.params.id},
+            req.body
+        );
+        res.send(task);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
  
 module.exports = router;
